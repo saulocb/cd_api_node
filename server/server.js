3538e09697590ea8fd870f93f96a454492993612
Saulo Santos <saulo.candido.b@hotmail.com>
2020-12-24 18:34:19 +0000

    .

--- server/server.js
+++ server/server.js
@@ -13,6 +13,16 @@ var express = require('express')
 var appExpress = express()
 
 app.use(cors())
+var corsOptions = {
+  origin: 'http://localhost:3006',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use('*', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for only example.com.'})
+})
+
+
 const app = module.exports = loopback();
 
 
