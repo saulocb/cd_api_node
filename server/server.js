679e173ac2a7893bbc0558f14aed27a427dac304
Saulo Santos <saulo.candido.b@hotmail.com>
2020-12-25 23:55:24 +0000

    test

--- server/server.js
+++ server/server.js
@@ -23,6 +23,9 @@ const app = module.exports = loopback();
 
 app.use('*', cors(corsOptions),  function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next()
 })
 
