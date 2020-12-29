616f62c15e9ac09276c30e0e9724b967d3ae2784
Saulo Santos <saulo.candido.b@hotmail.com>
2020-12-29 01:54:07 +0000

    d

--- common/models/shift.js
+++ common/models/shift.js
@@ -1,12 +1,6 @@
 'use strict';
 
 module.exports = function (Shift) {
-  Shift.observe('before save', async function (ctx, next) {
-    const token = ctx.options && ctx.options.accessToken;
-    const userId = token && token.userId;
-    ctx.instance.$userId = userId
-   return next();
-  });
 
 
 };
