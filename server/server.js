dd6918d8d6164a02ab97311e31b39ebf04fbfd58
Saulo Santos <saulo.candido.b@hotmail.com>
2020-12-29 01:39:14 +0000

    qw

--- server/server.js
+++ server/server.js
@@ -10,10 +10,11 @@ const boot = require('loopback-boot');
 
 const app = module.exports = loopback();
 
-app.use(function(){
+app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next()
 })
 
 
